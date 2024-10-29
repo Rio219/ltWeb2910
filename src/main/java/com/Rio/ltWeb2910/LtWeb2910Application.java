a12ffa29f14e679912d9b21e7577234c6f59b99b
RioHoang <131158772+Rio219@users.noreply.github.com>
2024-10-29 13:41:37 +0700

    init commit

---
+++ src/main/java/com/Rio/ltWeb2910/LtWeb2910Application.java
@@ -0,0 +1,13 @@
+package com.Rio.ltWeb2910;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtWeb2910Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtWeb2910Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Rio/ltWeb2910/LtWeb2910ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rio.ltWeb2910;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtWeb2910ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
